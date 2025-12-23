406612f7c6fc6830d3ed301e6ce4af346b915b77
Mohammad Mahdi Mahboob <mahbom2@mcmaster.ca>
2025-12-23 18:38:26 -0500

    sumof3values.cpp

---
+++ SearchingSorting/sumof3values.cpp
@@ -0,0 +1,120 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define fs first
+#define fio ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define sc second
+#define pb push_back
+#define eb emplace_back
+#define edl '\n'
+#define pf push_front
+#define ppb pop_back()
+#define fr front()
+#define bk back()
+#define sbf setbuf(stdout, 0)
+#define ppf pop_front()
+#define tp top()
+#define ps push
+#define pp pop()
+#define fls fflush(stdout)
+#define qu queue
+#define st stack
+#define pq priority_queue
+#define fora(i, a, b) for (int i = (a); i < (b); ++i)
+#define forae(i, a, b) for (int i = (a); i <= (b); ++i)
+#define foras(i, a, b, c) for (int i = (a); i < (b); i += (c))
+#define foraes(i, a, b, c) for (int i = (a); i <= (b); i += (c))
+#define ford(i, a, b) for (int i = (a); i > (b); --i)
+#define forde(i, a, b) for (int i = (a); i >= (b); --i)
+#define fords(i, a, b, c) for (int i = (a); i > (b); i -= (c))
+#define fordes(i, a, b, c) for (int i = (a); i >= (b); i -= (c))
+#define forals(i, a, b, c) for (long long i = (a); i < (b); i += (c))
+#define foraels(i, a, b, c) for (long long i = (a); i <= (b); i += (c))
+#define fordls(i, a, b, c) for (long long i = (a); i > (b); i -= (c))
+#define fordels(i, a, b, c) for (long long i = (a); i >= (b); i -= (c))
+#define MOD (int) (1e9 + 7)
+
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef pair<int, ll> pil;
+typedef pair<ll, int> pli;
+typedef pair<ll, ll> pl;
+typedef pair<ull, ull> pull;
+typedef pair<int, pi> pii;
+typedef pair<ll, pl> pll;
+typedef pair<pi, pi> ppi;
+typedef pair<char, int> pci;
+
+inline ll mulMod(ll x, ll y, ll m = LONG_LONG_MAX) {
+    x %= m;
+    ll res = 0;
+    while (y) {
+        if (y & 1) res = (res + x) % m;
+        if (res < 0) res += m;
+        x = (x << 1) % m;
+        y >>= 1;
+    }
+    return res % m;
+}
+
+inline ll logPow(ll x, ll y, ll m = LONG_LONG_MAX) {
+    x %= m;
+    ll res = 1;
+    while (y) {
+        if (y & 1) res = mulMod(res, x, m);
+        x = mulMod(x, x, m);
+        y >>= 1;
+    }
+    return res % m;
+}
+
+inline ll fsLogPow(ll x, ll y) {
+    ll res = 1;
+    while (y) {
+        if (y & 1) res *= x;
+        x *= x;
+        y >>= 1;
+    }
+    return res;
+}
+
+inline ll pLogPow(ll x, ll y, ll m = 9223372036854775783) { return logPow(x, y % (m - 1), m); }
+
+int main() {
+    /**
+     * Similar to the 2SUM problem, except with 3 values. To approach this, fix one of the values and use two pointers
+     * method to find the other two values. To get the positions, use pair struct to hold both the value and the
+     * original index of the element in the array before sorting. After sorting, fix the ith index as the first value,
+     * and then run two pointers to the right of the ith index, as values to the left of i would have been considered
+     * during previous values of i.
+     *
+     */
+    fio;
+    int n;
+    ll x;
+    cin >> n >> x;
+    vector<pli> arr(n);
+    fora(i, 0, n) {
+        cin >> arr[i].fs;
+        arr[i].sc = i + 1;
+    }
+    sort(arr.begin(), arr.end());
+    fora(i, 0, n - 2) {
+        int l = i + 1, r = n - 1;
+        while (l < r) {
+            int sum = arr[i].fs + arr[l].fs + arr[r].fs;
+            if (sum == x) {
+                cout << arr[i].sc << ' ' << arr[l].sc << ' ' << arr[r].sc << edl;
+                return 0;
+            } else if (sum < x) {
+                l++;
+            } else {
+                r--;
+            }
+        }
+    }
+    cout << "IMPOSSIBLE" << edl;
+}

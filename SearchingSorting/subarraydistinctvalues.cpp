d121d378d182c351c09e5bdbf93219f94622b857
Mohammad Mahdi Mahboob <mahbom2@mcmaster.ca>
2025-12-23 18:38:25 -0500

    subarraydistinctvalues.cpp

---
+++ SearchingSorting/subarraydistinctvalues.cpp
@@ -0,0 +1,120 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define fs first
+#define fio ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define sc second
+#define pb push_back
+#define eb emplace_back
+#define edl '\n'
+#define pf push_front
+#define ppb pop_back()
+#define fr front()
+#define bk back()
+#define sbf setbuf(stdout, 0)
+#define ppf pop_front()
+#define tp top()
+#define ps push
+#define pp pop()
+#define fls fflush(stdout)
+#define qu queue
+#define st stack
+#define pq priority_queue
+#define fora(i, a, b) for (int i = (a); i < (b); ++i)
+#define forae(i, a, b) for (int i = (a); i <= (b); ++i)
+#define foras(i, a, b, c) for (int i = (a); i < (b); i += (c))
+#define foraes(i, a, b, c) for (int i = (a); i <= (b); i += (c))
+#define ford(i, a, b) for (int i = (a); i > (b); --i)
+#define forde(i, a, b) for (int i = (a); i >= (b); --i)
+#define fords(i, a, b, c) for (int i = (a); i > (b); i -= (c))
+#define fordes(i, a, b, c) for (int i = (a); i >= (b); i -= (c))
+#define forals(i, a, b, c) for (long long i = (a); i < (b); i += (c))
+#define foraels(i, a, b, c) for (long long i = (a); i <= (b); i += (c))
+#define fordls(i, a, b, c) for (long long i = (a); i > (b); i -= (c))
+#define fordels(i, a, b, c) for (long long i = (a); i >= (b); i -= (c))
+#define MOD (int) (1e9 + 7)
+
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef pair<int, ll> pil;
+typedef pair<ll, ll> pl;
+typedef pair<ull, ull> pull;
+typedef pair<int, pi> pii;
+typedef pair<ll, pl> pll;
+typedef pair<pi, pi> ppi;
+typedef pair<char, int> pci;
+
+inline ll mulMod(ll x, ll y, ll m = LONG_LONG_MAX) {
+    x %= m;
+    ll res = 0;
+    while (y) {
+        if (y & 1) res = (res + x) % m;
+        if (res < 0) res += m;
+        x = (x << 1) % m;
+        y >>= 1;
+    }
+    return res % m;
+}
+
+inline ll logPow(ll x, ll y, ll m = LONG_LONG_MAX) {
+    x %= m;
+    ll res = 1;
+    while (y) {
+        if (y & 1) res = mulMod(res, x, m);
+        x = mulMod(x, x, m);
+        y >>= 1;
+    }
+    return res % m;
+}
+
+inline ll fsLogPow(ll x, ll y) {
+    ll res = 1;
+    while (y) {
+        if (y & 1) res *= x;
+        x *= x;
+        y >>= 1;
+    }
+    return res;
+}
+
+inline ll pLogPow(ll x, ll y, ll m = 9223372036854775783) { return logPow(x, y % (m - 1), m); }
+
+int main() {
+    /**
+     * Need to use an expanding sliding window approach. By default, expand. As long all values are unique, keep
+     * expanding. Whenever this is the case, add to the running total the number of subarrays within the window which
+     * end with the element at the right endpoint. This value is just r - l + 1. If it is not the case, move the left
+     * pointer to the right by one index. At the next iteration, recalculate the number of unique elements in the window
+     * considering that the element just before the left pointer is no longer part of the window.
+     *
+     * To count the number of unique elements in the window, use a set.
+     */
+    fio;
+    int n;
+    int l = 0, r = 0;
+    ll total = 0;
+    unordered_set<int> frqs;
+    cin >> n;
+    vector<int> arr(n);
+    fora(i, 0, n) {
+        cin >> arr[i];
+    }
+    while (r < n) {
+        if (frqs.count(arr[r]) == 0) {
+            // Add the element to the set
+            frqs.insert(arr[r]);
+            total += r - l + 1;
+            r++;
+        } else {
+            // Contracting, so the left endpoint moved to the left by 1. Since this is the case, the element just before
+            // the window is no longer considered, so remove it from the tracked set.
+            frqs.erase(arr[l]);
+            l++;
+        }
+    }
+    cout << total << edl;
+    return 0;
+}

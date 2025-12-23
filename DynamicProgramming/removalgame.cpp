a2ca00ff5ddadceeb8d56d43af14e36d916dc57d
Mohammad Mahdi Mahboob <mahbom2@mcmaster.ca>
2025-12-23 18:38:30 -0500

    removalgame.cpp

---
+++ DynamicProgramming/removalgame.cpp
@@ -0,0 +1,106 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define fs first
+#define fio ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define sc second
+#define pb push_back
+#define eb emplace_back
+#define edl '\n'
+#define pf push_front
+#define ppb pop_back()
+#define fr front()
+#define bk back()
+#define sbf setbuf(stdout, 0)
+#define ppf pop_front()
+#define tp top()
+#define ps push
+#define pp pop()
+#define fls fflush(stdout)
+#define qu queue
+#define st stack
+#define pq priority_queue
+#define fora(i, a, b, c) for (int i = (a); i < (b); i += (c))
+#define forae(i, a, b, c) for (int i = (a); i <= (b); i += (c))
+#define ford(i, a, b, c) for (int i = (a); i > (b); i -= (c))
+#define forde(i, a, b, c) for (int i = (a); i >= (b); i -= (c))
+#define foral(i, a, b, c) for (long long i = (a); i < (b); i += (c))
+#define forael(i, a, b, c) for (long long i = (a); i <= (b); i += (c))
+#define fordl(i, a, b, c) for (long long i = (a); i > (b); i -= (c))
+#define fordel(i, a, b, c) for (long long i = (a); i >= (b); i -= (c))
+#define MOD (int) (1e9 + 7)
+
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef pair<int, ll> pil;
+typedef pair<ll, ll> pl;
+typedef pair<ull, ull> pull;
+typedef pair<int, pi> pii;
+typedef pair<ll, pl> pll;
+typedef pair<pi, pi> ppi;
+typedef pair<char, int> pci;
+
+inline ll mulMod(ll x, ll y, ll m = LONG_LONG_MAX) {
+    x %= m;
+    ll res = 0;
+    while (y) {
+        if (y & 1) res = (res + x) % m;
+        if (res < 0) res += m;
+        x = (x << 1) % m;
+        y >>= 1;
+    }
+    return res % m;
+}
+
+inline ll logPow(ll x, ll y, ll m = LONG_LONG_MAX) {
+    x %= m;
+    ll res = 1;
+    while (y) {
+        if (y & 1) res = mulMod(res, x, m);
+        x = mulMod(x, x, m);
+        y >>= 1;
+    }
+    return res % m;
+}
+
+inline ll fsLogPow(ll x, ll y) {
+    ll res = 1;
+    while (y) {
+        if (y & 1) res *= x;
+        x *= x;
+        y >>= 1;
+    }
+    return res;
+}
+
+inline ll pLogPow(ll x, ll y, ll m = 9223372036854775783) { return logPow(x, y % (m - 1), m); }
+
+int main() {
+    fio;
+    int n;
+    cin >> n;
+    vector<ll> nums(n);
+    // i, j, 1-indexed
+    // bestScore[i][j] = max(c[j - 1] + min(best[i - 2][j + 1], best[i - 2][j + 2]),
+    //                       c[j + i - 2] + min(best[i - 2][j], best[i - 2][j + 1]))
+
+    // This is because you have to see best of taking either end + worst of taking from 2 coins less than current choice
+    vector<vector<ll>> bestScore(n + 1, vector<ll>(n + 1, 0));
+    for (int i = 0; i < n; ++i) {
+        cin >> nums[i];
+    }
+    forae(i, 1, n, 1) {
+        bestScore[1][i] = nums[i - 1];
+    }
+    for (int i = 2; i <= n; ++i) {
+        for (int j = 1; j <= n - i + 1; ++j) {
+            bestScore[i][j] = max(nums[j - 1] + min(bestScore[i - 2][j + 1], bestScore[i - 2][j + 2]),
+                                  nums[j + i - 2] + min(bestScore[i - 2][j], bestScore[i - 2][j + 1]));
+        }
+    }
+    cout << bestScore[nums.size()][1] << edl;
+    return 0;
+}

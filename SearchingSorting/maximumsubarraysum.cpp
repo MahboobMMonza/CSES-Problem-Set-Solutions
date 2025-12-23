f55e444ea51f4a9cf059af5a02318f55482277af
Mohammad Mahdi Mahboob <mahbom2@mcmaster.ca>
2025-12-23 18:38:21 -0500

    maximumsubarraysum.cpp

---
+++ SearchingSorting/maximumsubarraysum.cpp
@@ -0,0 +1,109 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define fs first
+#define fio ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define sc second
+#define pb push_back
+#define eb emplace_back
+#define edl '\n'
+#define pf push_front
+#define ppb pop_back()
+#define fr front()
+#define bk back()
+#define sbf setbuf(stdout, 0)
+#define ppf pop_front()
+#define tp top()
+#define ps push
+#define pp pop()
+#define fls fflush(stdout)
+#define qu queue
+#define st stack
+#define pq priority_queue
+#define fora(i, a, b, c) for (int i = (a); i < (b); i += (c))
+#define forae(i, a, b, c) for (int i = (a); i <= (b); i += (c))
+#define ford(i, a, b, c) for (int i = (a); i > (b); i -= (c))
+#define forde(i, a, b, c) for (int i = (a); i >= (b); i -= (c))
+#define foral(i, a, b, c) for (long long i = (a); i < (b); i += (c))
+#define forael(i, a, b, c) for (long long i = (a); i <= (b); i += (c))
+#define fordl(i, a, b, c) for (long long i = (a); i > (b); i -= (c))
+#define fordel(i, a, b, c) for (long long i = (a); i >= (b); i -= (c))
+
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef pair<int, ll> pil;
+typedef pair<ll, ll> pl;
+typedef pair<ull, ull> pull;
+typedef pair<int, pi> pii;
+typedef pair<ll, pl> pll;
+typedef pair<pi, pi> ppi;
+typedef pair<char, int> pci;
+
+inline ll mulMod(ll x, ll y, ll m = LONG_LONG_MAX) {
+    x %= m;
+    ll res = 0;
+    while (y) {
+        if (y & 1) res = (res + x) % m;
+        if (res < 0) res += m;
+        x = (x << 1) % m;
+        y >>= 1;
+    }
+    return res % m;
+}
+
+inline ll logPow(ll x, ll y, ll m = LONG_LONG_MAX) {
+    x %= m;
+    ll res = 1;
+    while (y) {
+        if (y & 1) res = mulMod(res, x, m);
+        x = mulMod(x, x, m);
+        y >>= 1;
+    }
+    return res % m;
+}
+
+inline ll fsLogPow(ll x, ll y) {
+    ll res = 1;
+    while (y) {
+        if (y & 1) res *= x;
+        x *= x;
+        y >>= 1;
+    }
+    return res;
+}
+
+inline ll pLogPow(ll x, ll y, ll m = 9223372036854775783) { return logPow(x, y % (m - 1), m); }
+
+inline ull trunk(ull x) {
+    return (ull) (UINT32_MAX) & x;
+}
+
+bool cmp(ull a, ull b) {
+    return (trunk(a)) < (trunk(b));
+}
+
+int main() {
+    /**
+     * At each number, the maximum subarray that includes that number is either a continuation of the previous max
+     * subarray, or a completely new subarray. This is because subarrays are contiguous, meaning no values can be
+     * skipped. If it is better to skip that value, then the current subarray must end, and a new one will be
+     * considered. Keep track of the max value that is found in each case, and that will be the answer.
+     */
+    fio;
+    // const int MX = 2e5+5;
+    int n;
+    ll s, ans = LONG_LONG_MIN, w;
+    cin >> n;
+    cin >> w;
+    ans = s = w;
+    fora(i,1,n,1) {
+        cin >> w;
+        s = max(w + s, w);
+        ans = max(ans, s);
+    }
+    cout << ans << edl;
+    return 0;
+}

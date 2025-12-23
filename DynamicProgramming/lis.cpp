a17ad1dec1a46c2bdcfb3cd977803322ccb90ec5
Mohammad Mahdi Mahboob <mahbom2@mcmaster.ca>
2025-12-23 18:38:29 -0500

    lis.cpp

---
+++ DynamicProgramming/lis.cpp
@@ -0,0 +1,103 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define fs first
+#define fio ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define sc second
+#define pb push_back
+#define eb emplace_back
+#define edl '\n'
+#define pf push_front
+#define ppb pop_back()
+#define fr front()
+#define bk back()
+#define sbf setbuf(stdout, 0)
+#define ppf pop_front()
+#define tp top()
+#define ps push
+#define pp pop()
+#define fls fflush(stdout)
+#define qu queue
+#define st stack
+#define pq priority_queue
+#define fora(i, a, b, c) for (int i = (a); i < (b); i += (c))
+#define forae(i, a, b, c) for (int i = (a); i <= (b); i += (c))
+#define ford(i, a, b, c) for (int i = (a); i > (b); i -= (c))
+#define forde(i, a, b, c) for (int i = (a); i >= (b); i -= (c))
+#define foral(i, a, b, c) for (long long i = (a); i < (b); i += (c))
+#define forael(i, a, b, c) for (long long i = (a); i <= (b); i += (c))
+#define fordl(i, a, b, c) for (long long i = (a); i > (b); i -= (c))
+#define fordel(i, a, b, c) for (long long i = (a); i >= (b); i -= (c))
+
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef pair<int, ll> pil;
+typedef pair<ll, ll> pl;
+typedef pair<ull, ull> pull;
+typedef pair<int, pi> pii;
+typedef pair<ll, pl> pll;
+typedef pair<pi, pi> ppi;
+typedef pair<char, int> pci;
+
+inline ll mulMod(ll x, ll y, ll m = LONG_LONG_MAX) {
+    x %= m;
+    ll res = 0;
+    while (y) {
+        if (y & 1) res = (res + x) % m;
+        if (res < 0) res += m;
+        x = (x << 1) % m;
+        y >>= 1;
+    }
+    return res % m;
+}
+
+inline ll logPow(ll x, ll y, ll m = LONG_LONG_MAX) {
+    x %= m;
+    ll res = 1;
+    while (y) {
+        if (y & 1) res = mulMod(res, x, m);
+        x = mulMod(x, x, m);
+        y >>= 1;
+    }
+    return res % m;
+}
+
+inline ll fsLogPow(ll x, ll y) {
+    ll res = 1;
+    while (y) {
+        if (y & 1) res *= x;
+        x *= x;
+        y >>= 1;
+    }
+    return res;
+}
+
+inline ll pLogPow(ll x, ll y, ll m = 9223372036854775783) { return logPow(x, y % (m - 1), m); }
+
+int main() {
+    fio;
+    int n, x;
+    cin >> n;
+    multiset<int> seq;
+    multiset<int>::iterator it;
+    // Read each number, and add it to an ordered multiset (being a multiset is important for duplicates)
+    for (int i = 0; i < n; ++i) {
+        cin >> x;
+        // Add the value to the multiset
+        seq.insert(x);
+        // Find the value's position in the sequence, then go to the next value. If that value is not at the
+        // end of the array, erase it. This is because this added smaller value will replace it in the sequence.
+        // If the value technically isn't part of the LIS, it doesn't matter since the sz of the sequence hasn't
+        // changed, and we are still maintaining LIS[i] < LIS[i + 1].
+        it = seq.find(x);
+        it++;
+        if (it != seq.end()) {
+            seq.erase(it);
+        }
+    }
+    cout << seq.size() << edl;
+    return 0;
+}

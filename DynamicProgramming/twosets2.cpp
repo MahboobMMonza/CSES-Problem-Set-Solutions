035bd1c4700f8dca10f5dda782749f32a03e7444
Mohammad Mahdi Mahboob <mahbom2@mcmaster.ca>
2025-12-23 18:38:31 -0500

    twosets2.cpp

---
+++ DynamicProgramming/twosets2.cpp
@@ -0,0 +1,103 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define fs first
+#define fio ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define sc second
+#define pb push_back
+#define eb emplace_back
+#define edl '\n'
+#define pf push_front
+#define ppb pop_back()
+#define fr front()
+#define bk back()
+#define sbf setbuf(stdout, 0)
+#define ppf pop_front()
+#define tp top()
+#define ps push
+#define pp pop()
+#define fls fflush(stdout)
+#define qu queue
+#define st stack
+#define pq priority_queue
+#define fora(i, a, b, c) for (int i = (a); i < (b); i += (c))
+#define forae(i, a, b, c) for (int i = (a); i <= (b); i += (c))
+#define ford(i, a, b, c) for (int i = (a); i > (b); i -= (c))
+#define forde(i, a, b, c) for (int i = (a); i >= (b); i -= (c))
+#define foral(i, a, b, c) for (long long i = (a); i < (b); i += (c))
+#define forael(i, a, b, c) for (long long i = (a); i <= (b); i += (c))
+#define fordl(i, a, b, c) for (long long i = (a); i > (b); i -= (c))
+#define fordel(i, a, b, c) for (long long i = (a); i >= (b); i -= (c))
+#define MOD (int) (1e9 + 7)
+
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef pair<int, ll> pil;
+typedef pair<ll, ll> pl;
+typedef pair<ull, ull> pull;
+typedef pair<int, pi> pii;
+typedef pair<ll, pl> pll;
+typedef pair<pi, pi> ppi;
+typedef pair<char, int> pci;
+
+inline ll mulMod(ll x, ll y, ll m = LONG_LONG_MAX) {
+    x %= m;
+    ll res = 0;
+    while (y) {
+        if (y & 1) res = (res + x) % m;
+        if (res < 0) res += m;
+        x = (x << 1) % m;
+        y >>= 1;
+    }
+    return res % m;
+}
+
+inline ll logPow(ll x, ll y, ll m = LONG_LONG_MAX) {
+    x %= m;
+    ll res = 1;
+    while (y) {
+        if (y & 1) res = mulMod(res, x, m);
+        x = mulMod(x, x, m);
+        y >>= 1;
+    }
+    return res % m;
+}
+
+inline ll fsLogPow(ll x, ll y) {
+    ll res = 1;
+    while (y) {
+        if (y & 1) res *= x;
+        x *= x;
+        y >>= 1;
+    }
+    return res;
+}
+
+inline ll pLogPow(ll x, ll y, ll m = 9223372036854775783) { return logPow(x, y % (m - 1), m); }
+
+int main() {
+    int n, sum;
+    cin >> n;
+    sum = (n * n + n) / 2;
+    vector<int> ways(sum + 1);
+    if ((sum - 1) & 1) {
+        sum >>= 1;
+        ways[0] = 1;
+        // Count number of ways not using the last number in the range. Each subset pair either has the last number, or
+        // doesn't. Count the ones that don't, and you've counted all the pairs of subsets.
+        fora(i, 1, n, 1) {
+            forde(j, sum, i, 1) {
+                ways[j] += ways[j - i];
+                ways[j] %= MOD;
+                if (i == n - 1) {
+                    break;
+                }
+            }
+        }
+    }
+    cout << ways[sum] << edl;
+    return 0;
+}

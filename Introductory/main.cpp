8636475994f64294696e6094810dfe6091b08e00
Mohammad Mahdi Mahboob <mahbom2@mcmaster.ca>
2025-12-23 18:38:15 -0500

    main.cpp

---
+++ Introductory/main.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define fs first
+#define fio ios::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL)
+#define sc second
+#define pb push_back
+#define eb emplace_back
+#define edl '\n'
+#define pf push_front
+#define ppb pop_back()
+#define fr front()
+#define bk back()
+#define sbf setbuf(stdout, 0)
+#define ppf pop_front()
+#define tp top()
+#define ps push
+#define pp pop()
+#define fls fflush(stdout)
+#define qu queue
+#define st stack
+#define pq priority_queue
+#define fora(i, a, b, c) for (int (i) = (a); (i) < (b); (i) += (c))
+#define forae(i, a, b, c) for (int (i) = (a); (i) <= (b); (i) += (c))
+#define ford(i, a, b, c) for (int (i) = (a); (i) > (b); (i) -= (c))
+#define forde(i, a, b, c) for (int (i) = (a); (i) >= (b); (i) -= (c))
+#define dblinf numeric_limits<double>::infinity()
+
+typedef unsigned long long ull;
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef pair<int, ll> pil;
+typedef pair<ll, ll> pl;
+typedef pair<ull, ull> pull;
+typedef pair<int, pi> pii;
+typedef pair<ll, pl> pll;
+typedef pair<pi, pi> ppi;
+typedef pair<double, double> pd;
+typedef pair<char, int> pci;
+
+int main() {
+    fio;
+    set<ll> multiples;
+    ll ans = 0, k, n, m;
+    int l = 0, r = 1;
+    vector<ll> notes(1e6 + 2);
+    cin >> n >> m >> k;
+
+    return 0;
+}

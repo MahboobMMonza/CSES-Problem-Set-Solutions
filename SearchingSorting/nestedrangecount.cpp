0f545ba6f38cb258bc61d7eaef397a3fd71ae305
Mohammad Mahdi Mahboob <mahbom2@mcmaster.ca>
2025-12-27 00:28:56 -0500

    nestedrangecount.cpp

---
+++ SearchingSorting/nestedrangecount.cpp
@@ -0,0 +1,173 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define fs first
+#define fio ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define sc second
+#define pb push_back
+#define eb emplace_back
+#define edl '\n'
+#define pf push_front
+#define ppb pop_back()
+#define fr front()
+#define bk back()
+#define sbf setbuf(stdout, 0)
+#define ppf pop_front()
+#define tp top()
+#define ps push
+#define pp pop()
+#define fls fflush(stdout)
+#define qu queue
+#define st stack
+#define pq priority_queue
+#define fora(i, a, b) for (int i = (a); i < (b); ++i)
+#define forae(i, a, b) for (int i = (a); i <= (b); ++i)
+#define foras(i, a, b, c) for (int i = (a); i < (b); i += (c))
+#define foraes(i, a, b, c) for (int i = (a); i <= (b); i += (c))
+#define ford(i, a, b) for (int i = (a); i > (b); --i)
+#define forde(i, a, b) for (int i = (a); i >= (b); --i)
+#define fords(i, a, b, c) for (int i = (a); i > (b); i -= (c))
+#define fordes(i, a, b, c) for (int i = (a); i >= (b); i -= (c))
+#define forals(i, a, b, c) for (long long i = (a); i < (b); i += (c))
+#define foraels(i, a, b, c) for (long long i = (a); i <= (b); i += (c))
+#define fordls(i, a, b, c) for (long long i = (a); i > (b); i -= (c))
+#define fordels(i, a, b, c) for (long long i = (a); i >= (b); i -= (c))
+#define foraz(i, a, b) for (size_t i = (a); i < (b); ++i)
+#define foraez(i, a, b) for (size_t i = (a); i <= (b); ++i)
+#define fordz(i, a, b) for (size_t i = (a); i > (b); --i)
+#define fordez(i, a, b) for (size_t i = (a); i >= (b); --i)
+#define ford0z(i, a, b) for (size_t i = (a); i < (b); --i)
+#define forazs(i, a, b, c) for (size_t i = (a); i < (b); i += (c))
+#define foraezs(i, a, b, c) for (size_t i = (a); i <= (b); i += (c))
+#define fordzs(i, a, b, c) for (size_t i = (a); i > (b); i -= (c))
+#define fordezs(i, a, b, c) for (size_t i = (a); i >= (b); i -= (c))
+#define ford0zs(i, a, b, c) for (size_t i = (a); i < (b); i -= (c))
+#define MOD (int) (1e9 + 7)
+
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef pair<int, ll> pil;
+typedef pair<ll, ll> pl;
+typedef pair<ull, ull> pull;
+typedef pair<int, pi> pii;
+typedef pair<ll, pl> pll;
+typedef pair<pi, pi> ppi;
+typedef pair<char, int> pci;
+
+struct custom_hash {
+    static ull splitmix64(ull x) {
+        // http://xorshift.di.unimi.it/splitmix64.c
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+
+    ull operator()(ull x) const {
+        static const ull FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+
+template<typename T>
+using indexed_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template<typename T, typename K>
+using indexed_map = tree<T, K, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+inline ll mulMod(ll x, ll y, ll m = LONG_LONG_MAX) {
+    x %= m;
+    ll res = 0;
+    while (y) {
+        if (y & 1) res = (res + x) % m;
+        if (res < 0) res += m;
+        x = (x << 1) % m;
+        y >>= 1;
+    }
+    return res % m;
+}
+
+inline ll logPow(ll x, ll y, ll m = LONG_LONG_MAX) {
+    x %= m;
+    ll res = 1;
+    while (y) {
+        if (y & 1) res = mulMod(res, x, m);
+        x = mulMod(x, x, m);
+        y >>= 1;
+    }
+    return res % m;
+}
+
+struct endpoint {
+    ull end;
+    size_t idx;
+
+    endpoint(const ull end, const size_t idx) : end(end), idx(idx) {}
+
+    bool operator<(const endpoint& rhs) const {
+        return end == rhs.end ? idx > rhs.idx : end < rhs.end;
+    }
+};
+
+inline ll fsLogPow(ll x, ll y) {
+    ll res = 1;
+    while (y) {
+        if (y & 1) res *= x;
+        x *= x;
+        y >>= 1;
+    }
+    return res;
+}
+
+inline ll pLogPow(ll x, ll y, ll m = 9223372036854775783) { return logPow(x, y % (m - 1), m); }
+
+bool cmp(const pii& a, const pii& b) {
+    return a.fs == b.fs ? a.sc.fs > b.sc.fs : a.fs < b.fs;
+}
+
+int main() {
+    /**
+     * Similar to nested ranges check, except this time we store the set of right endpoints existing after us. To see
+     * how many ranges contain the current range, we iterate the sorted ranges list and count how many right endpoints
+     * exist that are >= to the current range's right endpoint.
+     *
+     * To get how many ranges the current range contains, iterate the list in reverse order, and count how many
+     * endpoints are <= to the current endpoint.
+     *
+     * Use indexed treeset to count how many values <= current one (order of key).
+     */
+    fio;
+    size_t n;
+    cin >> n;
+    vector<pii> ranges(n);
+    vector<int> ans(n);
+    foraz(i, 0, n) {
+        cin >> ranges[i].fs >> ranges[i].sc.fs;
+        ranges[i].sc.sc = i;
+    }
+    sort(ranges.begin(), ranges.end(), cmp);
+    indexed_set<endpoint> range_ends;
+    ford0z(i, n - 1, n) {
+        const endpoint p(ranges[i].sc.fs, i);
+        range_ends.insert(p);
+        ans[ranges[i].sc.sc] = static_cast<int>(range_ends.order_of_key(p));
+    }
+    foraz(i, 0, n) {
+        cout << ans[i] << ' ';
+    }
+    range_ends.clear();
+    foraz(i, 0, n) {
+        const endpoint p(ranges[i].sc.fs, i);
+        range_ends.insert(p);
+        ans[ranges[i].sc.sc] = static_cast<int>(range_ends.size() - range_ends.order_of_key(p) - 1);
+    }
+    cout << edl;
+    foraz(i, 0, n) {
+        cout << ans[i] << ' ';
+    }
+    return 0;
+}
